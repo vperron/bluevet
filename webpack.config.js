84875046c848ec01ce5a1767f361dc9af070093b
Victor Perron <victor.perron@polyconseil.fr>
2017-02-26 12:24:21 +0100

    Point moment to cassets's moment, and ignore jwt simple with a hack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const webpackDefaults = require('systematic').webpack_get_defaults(__dirname)
 
+webpackDefaults.resolve.alias = {
+  moment: 'cassets/scripts/moment',
+  'jwt-simple': 'cassets/scripts/moment',
+}
+
 webpackDefaults.devtool = 'eval'
 
 module.exports = webpackDefaults
